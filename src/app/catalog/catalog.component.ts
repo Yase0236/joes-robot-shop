d99825f396165af12f51db97796add322825e753
Jasmin Ersan <A504654@dekra.org>
2024-03-08 13:12:48 +0100

    commit

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product.model';
+import { CartService } from '../cart/cart.service';
+import { ProductService } from './product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'bot-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent {
+  // Changed the property to an array data type
+  // The ! tells TS to ignore that it's not initialized and assume it always has a value
+  products: IProduct[] = [];
+  // Filter property for button filters
+  filter: string = '';
+
+  constructor(
+    private cartSvc: CartService,
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.productSvc.getProducts().subscribe(products => {
+      // products to the right of the assignment operator is the data coming from the server, and it's being assigned to the products property of the catalog.
+      this.products = products;
+    });
+     // Instead of using snapshot, we really want to setup a subscription that listens to changes to the route parameters.
+    this.route.queryParams.subscribe((params) => {
+        // If a filter isn't provided, set the filter to an empty string
+      this.filter = params['filter'] ?? '';
+    })
+  }
+
+  addToCart(product: IProduct) {
+    this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
+  }
+
+  getDiscountedClasses(product: IProduct) {
+    // Return the strikethrough object
+    if (product.discount > 0) return ['strikethrough'];
+    else return [];
+  }
+
+  // if else statement
+  getFilteredProducts() {
+    // If the filter is an empty string
+    return this.filter === ''
+       // Return the entire list
+      ? this.products
+      // Otherwise return the filtered list of products where the category equals this filter property
+      : this.products.filter(
+        (product) => product.category === this.filter
+      );
+  }
+}
